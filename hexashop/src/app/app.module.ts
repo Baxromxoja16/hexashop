ded1b46fdfecd1b93b48c2f781884447ac4830c0
baxromxoja16 <aglamof@mail.ru>
2023-07-01 11:26:43 +0500

    feat: add layout

--- hexashop/src/app/app.module.ts
+++ hexashop/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { SingleProductPageComponent } from './single-product-page/single-product-page.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    ProductsPageComponent,
+    SingleProductPageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
