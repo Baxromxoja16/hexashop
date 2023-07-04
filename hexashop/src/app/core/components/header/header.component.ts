1696a295250fdf6993ae3041c599441f63e95f8e
baxromxoja16 <aglamof@mail.ru>
2023-07-04 21:02:57 +0500

    fix: header count cart data

--- hexashop/src/app/core/components/header/header.component.ts
+++ hexashop/src/app/core/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { HomeService } from 'src/app/home-page/services/home.service';
+import { ProductsMoreService } from '../../services/products-more.service';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,9 @@ export class HeaderComponent {
 
   subscription$: Subscription = new Subscription();
 
-  constructor(private router: Router, private homeService: HomeService){}
+  countCarts = this.productsMoreService.cardData;
+
+  constructor(private router: Router, private homeService: HomeService, private productsMoreService: ProductsMoreService){}
 
 
   onDiscoverMore(id: string) {
