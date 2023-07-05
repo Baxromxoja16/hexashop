0b42e0f0a8586aeb9234164d5deacda3e72f26f2
boburf <changeitgo3@gmail.com>
2023-07-05 21:13:47 +0500

    feat: entry points and swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api example')
+    .setDescription('The Hexashop API description')
+    .setVersion('1.0')
+    .addTag('Hexashop')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
