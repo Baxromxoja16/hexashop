fcd4f1964a5571bb8a3f42e61ff50e5162557c55
baxromxoja16 <aglamof@mail.ru>
2023-07-06 16:26:35 +0500

    feat: add functional to category page

---
+++ hexashop/src/app/products-page/services/products.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  baseUrl = 'http://localhost:3004/'
+
+  constructor(private http: HttpClient) {}
+
+  getCategoryProducts() {
+    return this.http.get(this.baseUrl + 'goods/category/clothes/');
+  }
+}
