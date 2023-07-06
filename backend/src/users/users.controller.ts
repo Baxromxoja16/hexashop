9923f711fbd0f5376526d9ec18a4e280f9598e57
boburf <changeitgo3@gmail.com>
2023-07-06 12:35:52 +0500

    fix: some api points

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,14 +1,10 @@
 import {
   Controller,
   Get,
-  Body,
-  Patch,
   Param,
-  Delete,
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard, Public } from 'src/auth/guards/auth.guard';
 
 @UseGuards(AuthGuard)
@@ -29,15 +25,5 @@ export class UsersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
   }
 }
