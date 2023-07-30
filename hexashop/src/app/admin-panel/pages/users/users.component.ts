4e0bd1d0069b72078e272ce3e126170ed70a54ef
baxromxoja16 <aglamof@mail.ru>
2023-07-30 17:59:30 +0500

    feat: add users functional

--- hexashop/src/app/admin-panel/pages/users/users.component.ts
+++ hexashop/src/app/admin-panel/pages/users/users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  users!: any;
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe(users => {
+      console.log(users);
+      this.users = users
+    })
+  }
 }
