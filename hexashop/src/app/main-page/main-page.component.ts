b7b19fd6335b963a2e39f89a43018ae41db5e185
baxromxoja16 <aglamof@mail.ru>
2023-08-25 16:26:33 +0500

    fix: message

--- hexashop/src/app/main-page/main-page.component.ts
+++ hexashop/src/app/main-page/main-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent  implements OnInit {
+
+  ngOnInit(): void {
+    alert('При первом посещении данные будут отображаться немного медленнее, пожалуйста, подождите немного');
+  }
 }
