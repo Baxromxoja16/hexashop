5e29959cffeaa3aacc6eb5c8b41d012f99185de5
baxromxoja16 <aglamof@mail.ru>
2023-08-25 16:20:16 +0500

    feat: add message

--- hexashop/src/app/app.component.ts
+++ hexashop/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hexashop';
+
+  ngOnInit(): void {
+    alert('При первом посещении данные будут отображаться немного медленнее, пожалуйста, подождите немного');
+  }
 }
